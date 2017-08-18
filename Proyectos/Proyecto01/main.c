007001bf8403332b025323b854418589717ce851
ricardorodab <ricardorodab19@gmail.com>
2017-08-17 19:44:45 -0500

    Ejemplo de documentacion y licencia

---
+++ Proyectos/Proyecto01/main.c
@@ -0,0 +1,61 @@
+/* -----------------------------------------------------------------
+ * main.c
+ * version 1.0
+ * Copyright (C) 2017  Jose Ricardo Rodriguez Abreu.
+ * Facultad de Ciencias,
+ * Universidad Nacional Autonoma de Mexico, Mexico.
+ *
+ * Este programa es software libre; se puede redistribuir
+ * y/o modificar en los terminos establecidos por la
+ * Licencia Publica General de GNU tal como fue publicada
+ * por la Free Software Foundation en la version 2 o
+ * superior.
+ *
+ * Este programa es distribuido con la esperanza de que
+ * resulte de utilidad, pero SIN GARANTIA ALGUNA; de hecho
+ * sin la garantia implicita de COMERCIALIZACION o
+ * ADECUACION PARA PROPOSITOS PARTICULARES. Vease la
+ * Licencia Publica General de GNU para mayores detalles.
+ *
+ * Con este programa se debe haber recibido una copia de la
+ * Licencia Publica General de GNU, de no ser asi, visite el
+ * siguiente URL:
+ * http://www.gnu.org/licenses/gpl.html
+ * o escriba a la Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * -----------------------------------------------------------------
+ */
+
+/**
+ * @file main.c
+ * @author Jose Ricardo Rodriguez Abreu
+ * @date 17 Ago 2017
+ * @brief File containing just dummy main funtion.
+ *
+ * El uso de este programa es ejemplificar los comentarios.
+ *
+ * El programa usa el estandar de documentacion que define el uso de 
+ * doxygen.
+ *
+ * @see http://www.stack.nl/~dimitri/doxygen/manual/index.html
+ * @see https://github.com/ricardorodab/EjerciciosEjemploModelado
+ *
+ */
+
+
+/**
+ * @brief Metodo principal del programa.
+ *
+ * Ejemplo de comentario de una funcion
+ * @param argc - Es el numero de parametros. 
+ * En este caso se espera argc == 0.
+ * @param argv - Son los parametros NULL.
+ * @return Un entero 0 si todo sale bien 1 en caso contrario.
+ * @TODO Aqui puede ir algo que me falta implementar (to do).
+ *
+ */
+
+int main(void)
+{
+  return 0;
+}
